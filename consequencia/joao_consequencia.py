5c427fbb2459cfe4aa0a2b2ff9507401a39fa83f
Gabriel-em-Vocação <00001118493928sp@al.educacao.sp.gov.br>
2025-10-20 16:55:31 -0300

    Create joao_consequencia.py
    lista de desejos

---
+++ consequencia/joao_consequencia.py
@@ -0,0 +1,22 @@
+ lista de desejos
+ 
+
+'''
+joao_consequencia.py = "meus_desejos.txt"
+desejos = [ passar em direito]
+abra com ('joao_consequencia.py, r, enconding = utf-8')
+print(" meus desejos foram carregados do arquivo [ joao_consequencia.py]´'\n")
+
+print("parece que é sua primeira vez! vamos criar sua lista de desejos.\n`")
+
+print(f" ocorreu um erro em carregar sua lista de desejos: {E}")
+#---- funçao para salvar os desejos no arquivo---
+def salvar_desejos(lista_de_desejos)
+
+# "w" significa modo de escrita (write)
+# "e" apaga o conteudo existente se o arquivo existir
+# ´enconding = "utf-8" é importnte para caracteristicas especiais (acentos e emojis)
+
+abra com ( joao_consequencia.py, "w", enconding = "utf-8")
+
+arquivo.write (desejo + "\n")
